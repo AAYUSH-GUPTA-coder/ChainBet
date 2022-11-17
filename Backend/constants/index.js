7c81efbc3f7176dbb0d145d97a5afa4728c6e691
AAYUSH-GUPTA-coder <60979345+AAYUSH-GUPTA-coder@users.noreply.github.com>
2022-11-17 19:13:35 +0530

    remove Backend

--- Backend/constants/index.js
+++
@@ -1,7 +0,0 @@
-const { ethers } = require("hardhat");
-
-const FEE = ethers.utils.parseEther("1");
-const INTERVAL = 3600; // for production
-// const INTERVAL = 60; // for testing
-
-module.exports = { INTERVAL, FEE };
